0bdb85d7bcfd71084f3093dc20bb55a6bef43ef7
Judith Ofosu <61045983+jsofosu@users.noreply.github.com>
2022-01-21 11:27:15 +0000

    Added the CORS access

--- src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
+++ src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
@@ -3,9 +3,12 @@ package io.turntabl.leaderboardservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
 @EnableScheduling
+@CrossOrigin(origins = "http://localhost:4200")
 public class LeaderboardServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/turntabl/leaderboardservice/controller/LeaderboardController.java
+++ src/main/java/io/turntabl/leaderboardservice/controller/LeaderboardController.java
@@ -2,6 +2,7 @@ package io.turntabl.leaderboardservice.controller;
 
 import io.turntabl.leaderboardservice.controller.response.ProfileDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("v1/leaderboard")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LeaderboardController {
 
     private final LeaderboardFacade leaderboardFacade;
